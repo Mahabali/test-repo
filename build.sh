5812b4ba75f6c5fab315c3acf2be2fac29f14d49
mahabali3004@gmail.com <mahabali3004@gmail.com>
2022-09-28 18:56:44 +0530

    add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sonar-scanner \
+  -Dsonar.projectKey=test \
+  -Dsonar.sources=src \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=sqp_3ad05e4384e8f9f18378247b46af377d8f3d17df
+
+return 0
